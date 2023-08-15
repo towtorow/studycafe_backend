ac745604852663967c8a5620223b28583c5b1302
박성빈 <bkllnmas@naver.com>
2023-08-15 19:55:59 +0900

    Revert "API 인증 전부 허용 설정"
    This reverts commit ff2b48397e10955eb589d1d2d557d6ed6db5c6c1.

--- src/main/java/com/studycafe/studycafe_backend/controller/MainController.java
+++ src/main/java/com/studycafe/studycafe_backend/controller/MainController.java
@@ -1,21 +1,12 @@
 package com.studycafe.studycafe_backend.controller;
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.LinkedHashMap;
-import java.util.Map;
-@Slf4j
 @RestController
-@RequestMapping("/api/v1")
 public class MainController {
     @GetMapping("/try")
-    public Map<String, Object> root() {
-        Map<String, Object> rtnMap = new LinkedHashMap<String, Object>();
-        rtnMap.put("msg", "connected!");
-        return rtnMap;
+    public String root() {
+        return "연결 성공!";
     }
 }
